6cbfbe7cf6ee13f1fc94ec773a4ee90e98e7d8b3
Tuncay Aydin <tunikb@posteo.de>
2019-08-12 19:38:56 +0200

    style: major code refactoring done to enhance readibility style: remove unnecessary code

--- js/app.js
+++ js/app.js
@@ -1,39 +1,65 @@
+const minY = -35;         // minimum y-axis pixel
+const stepSizeY = 83;     // amount of pixels per step the player moves on y-axis
+const stepSizeX = 101;    // amount of pixels per step the player moves on x-axis
+const minX = 0;           // minimum x-axis pixel
+const maxX = 505;         // maximum x-axis pixel
+const maxY = 606;         // maximum y-axis pixel
+const enemyStartX = -200; // starting point of enemies on the x-axis
+const collisionRange = 50; // range of pixels around player sprite that triggers collision
+const laneCountY = 5;     // total lanes on y-axis
+const laneCountX = 6;     // total lanes on x-axis
+const allEnemies = [];    // array that stores all enemies
+const enemyCount = 5;     // amount of enemies in game
+const minSpeed = 40;      // minium speed of enemy in pixels
+const maxSpeed = 300;     // maximum speed of enemy in pixels
+const allEnemyPosY = [48, 131, 214]; // all possible lanes on which enemies can spawn
+let points = 0;           // points counter for player score
 
-const bodyColor = document.querySelector('body');
 
 class Enemy {
-  constructor(x, y, movementSpeed) {
-    this.x = x;
-    this.y = y;
+  constructor() {
+    // everthing thats added to new enemy objects
+    this.resetEnemy();
     this.sprite = 'images/enemy-bug.png';
-    this.movementSpeed = movementSpeed;
+  }
+  resetEnemy() {
+    // resets enemy x position, randomizes y position from possible lanes and randomizes
+    // speed in which the enemy moves through the lane
+    this.x = enemyStartX;
+    this.y = allEnemyPosY[Math.floor(Math.random() * allEnemyPosY.length)];
+    this.movementSpeed = Math.floor(Math.random() * (maxSpeed - minSpeed)) + minSpeed;
+  }
+  handleCollision() {
+    // when collision is detected
+    // remove one player life
+    player.playerLifes -= 1;
+    // reset player position
+    player.x = stepSizeX * 2;
+    player.y = minY + stepSizeY*5;
+    // change stars accordingly
+    const stars = document.querySelectorAll('.fa-star');
+    stars[player.playerLifes].classList.add('fa-star-o');
+    if (player.playerLifes === 0) {
+      // if player has no stars left
+      // show game over modal
+      document.querySelector('.modal').style.display = 'block';
+    }
   }
   update(dt) {
-    // collision handler checks if
-    if (Math.round(this.x) + 50 > player.x &&
-        Math.round(this.x) < player.x + 50 &&
-        50 + player.y > this.y &&
-        player.y < this.y + 50) {
-          if (document.querySelectorAll('.fa-star')[1].classList.contains('fa-star-o')) {
-            document.querySelectorAll('.fa-star')[0].classList.add('fa-star-o');
-            document.querySelector('.modal').style.display = 'block';
-          }
-          if (document.querySelectorAll('.fa-star')[2].classList.contains('fa-star-o')) {
-            document.querySelectorAll('.fa-star')[1].classList.add('fa-star-o');
-          }
-          else {
-          document.querySelectorAll('.fa-star')[2].classList.add('fa-star-o');
-          }
-          player.x = 101 * 2;
-          player.y = -35 + 83*5;
-        }
+    // determine how far enemy sprites move forward
     this.x += this.movementSpeed * dt;
-    if (this.x > 505) {
-      let enemyPosY = allEnemyPosY[Math.floor(Math.random() * allEnemyPosY.length)];
-      let movementSpeed = Math.floor(Math.random() * (maxSpeed - minSpeed)) + minSpeed;
-      this.movementSpeed = movementSpeed;
-      this.y = enemyPosY;
-      this.x = -200;
+    // collision is detected by checking player position plus collision range against enemy position
+    const collisionDetected = Math.round(this.x) + collisionRange > player.x &&
+                              Math.round(this.x) < player.x + collisionRange &&
+                              collisionRange + player.y > this.y &&
+                              player.y < this.y + collisionRange;
+
+    if (collisionDetected) {
+      this.handleCollision();
+    }
+    if (this.x > maxX) {
+      // when enemy leaves visible lane reset position to the beginning and reset speed
+      this.resetEnemy();
     }
   }
   render() {
@@ -42,9 +68,9 @@ class Enemy {
   }
 }
 
-let points = 0;
 
 let raisePoints = () => {
+  // raise points and modify presentation accordingly
   points += 5;
   document.getElementsByClassName('points')[0].innerText = points;
   document.getElementsByClassName('points')[1].innerText = points;
@@ -52,17 +78,19 @@ let raisePoints = () => {
 
 class Player {
   constructor() {
-    // initial position for the player character
-    this.x = 101 * 2;
-    this.y = -35 + 83*5;
+    // initial position for the player character, player sprite and lifes
+    this.x = stepSizeX * 2;
+    this.y = minY + stepSizeY * 5;
     this.sprite = 'images/char-boy.png';
+    this.playerLifes = 3;
   }
   update() {
-    if (this.y === -35 + 83 * 0) {
+    if (this.y === minY + stepSizeY * 0) {
+      // if player reaches top lane (winning lane) reset position after 10 miliseconds
       setTimeout(() => {
         raisePoints();
-        this.x = 101 * 2;
-        this.y = -35 + 83*5;
+        this.x = stepSizeX * 2;
+        this.y = minY + stepSizeY * 5;
       }, 10);
     }
   }
@@ -73,38 +101,31 @@ class Player {
     // stuff
   handleInput(keyName) {
     // keyboard inputs move the player character accordingly
-    if (keyName === 'left' && this.x !== 0) {
-      this.x -= 101;
+    // if player is on either border of the canvas, don't do anything (block movement)
+    if (keyName === 'left' && this.x !== minX) {
+      this.x -= stepSizeX;
     };
-    if (keyName === 'right' && this.x !== 101 * 4) {
-      this.x += 101;
+    if (keyName === 'right' && this.x !== stepSizeX * 4) {
+      this.x += stepSizeX;
     };
-    if (keyName === 'down' && this.y !== -35 + 83 * 5) {
-      this.y += 83;
+    if (keyName === 'down' && this.y !== minY + stepSizeY * 5) {
+      this.y += stepSizeY;
     };
-    if (keyName === 'up' && this.y !== -35) {
-      this.y -= 83;
+    if (keyName === 'up' && this.y !== minY) {
+      this.y -= stepSizeY;
     };
   };
 };
 
-let allEnemies = [];
-let player =  new Player();
-let enemyCount = 5;
-const minSpeed = 40;
-const maxSpeed = 300;
-const allEnemyPosY = [48, 131, 214];
-const enemyType = ['slime', 'skurr']
+const player =  new Player();
 
 for (let i = 0; i < enemyCount; i++) {
-  let enemyPosY = allEnemyPosY[Math.floor(Math.random() * allEnemyPosY.length)];
-  let movementSpeed = Math.floor(Math.random() * (maxSpeed - minSpeed)) + minSpeed;
-  let enemy = new Enemy(-300, enemyPosY, movementSpeed);
+  // create enemies according to the enemy count
+  let enemy = new Enemy();
   allEnemies.push(enemy);
 }
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -117,13 +138,17 @@ document.addEventListener('keyup', function(e) {
 });
 
 const winningScreenRestart = document.querySelector('.restartButton');
+// restart button put in a variable
 
 winningScreenRestart.addEventListener('click', function(e){
+  // when restart button is clicked, restore starting state by adding all stars
+  // make the modal disappear, reset points and add all player lifes
   document.querySelectorAll('.fa-star')[0].classList.remove('fa-star-o');
   document.querySelectorAll('.fa-star')[1].classList.remove('fa-star-o');
   document.querySelectorAll('.fa-star')[2].classList.remove('fa-star-o');
   document.querySelector('.modal').style.display = 'none';
   points = 0;
+  player.playerLifes = 3;
   document.getElementsByClassName('points')[0].innerText = points;
   document.getElementsByClassName('points')[1].innerText = points;
 });
